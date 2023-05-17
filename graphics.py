944ee9db257bf05ef35173fd4351388a49133c00
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-05-17 22:19:06 +0530

    Separated layout calculation and drawing into separate functions

--- graphics.py
+++ graphics.py
@@ -4,6 +4,19 @@ import browser
 WIDTH, HEIGHT = 800, 600
 
 
+def layout(text):
+    display_list = []
+    HSTEP, VSTEP = 13, 18
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
+
+
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
@@ -14,18 +27,15 @@ class Browser:
         )
         self.canvas.pack()
 
+    def draw(self):
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y, text=c)
+
     def load(self, url):
-        # Show the widgets
         headers, body = browser.request(url)
         text = browser.lex(body)
-        HSTEP, VSTEP = 13, 18
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+        self.display_list = layout(text)
+        self.draw()
 
 
 if __name__ == "__main__":
