d25a8026944805327a94a7b9b20740e452cb2346
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-05-19 14:15:37 +0530

    Implemented move to the next line on \n

--- graphics.py
+++ graphics.py
@@ -10,6 +10,10 @@ def layout(text):
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
+        if c == '\n':
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+            continue
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += HSTEP
         if cursor_x >= WIDTH - HSTEP:
